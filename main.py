158967351b50f872606a3a070691a04b59ae0ea0
VarienceDSC <unfoundedhearts@gmail.com>
2023-12-10 19:55:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import time
 def sendsms(Phone,content):
     time.sleep(5)
     os.shell(f"adb shell am start -a android.intent.action.SENDTO -d sms:+620{Phone} --es sms_body {content}")
-
+    time.sleep(2)
+    os.shell("adb shell input keyevent KEYCODE_TAB")
+    os.shell("adb shell input keyevent KEYCODE_TAB")
+    time.sleep(1)
+    os.shell("adb shell input keyevent KEYCODE_ENTER")
 
 
 def start() :
